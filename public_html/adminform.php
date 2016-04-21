ee003373c33006adc2efbdb42ad4c6a323bc7191
Nerukin <xen-oh@hotmail.com>
2016-04-21 16:48:14 -0400

    and welcome to the JAM!

---
+++ public_html/adminform.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+   <head>
+   </head>
+
+   <body>
+
+     <form onsubmit="actualizarcolegio()">
+        <label for="nombrecolegio">Nombre Colegio:</label><br>
+        <input id="nombrecolegio" type="text" name="nombrecolegio"><br>
+        <label for="director">Nombre Director:</label><br>
+        <input id="director" type="text" name="director"><br>
+        <label for="sostenedor">Nombre Sostenedor:</label><br>
+        <input id="sostenedor" type="text" name="sostenedor"><br><br>
+        <label for="mensualidad">Mensualidad:</label><br>
+        <select name="mensualidad">
+          <option value="1">Gratuito</option>
+          <option value="2">De $1.000 a $10.000</option>
+          <option value="3">De $10.001 a $25.000</option>
+          <option value="4">De $25.001 a $50.000</option>
+          <option value="5">De $50.001 a $100.000</option>
+          <option value="6">Mas de $100.000</option>
+          <option value="7">Sin informacion</option>
+        </select><br><br>
+        <label for="dependencia">Dependencia:</label><br>
+        <select name="dependencia">
+          <option value="1">Municipal</option>
+          <option value="2">Partucular Subvencionado</option>
+          <option value="3">Particular Pagado</option>
+          <option value="4">Otro</option>
+        </select><br><br>
+        <input type="submit" value="Enviar">
+     </form>
+
+   </body>
+
+</html>
---
+++ public_html/puntaje.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+   <head>
+   </head>
+
+   <body>
+
+     <form onsubmit="actualizarpuntaje()">
+       <label for="puntaje">Puntaje:</label><br>
+       <input id="puntaje" type="text" name="puntaje"><br>
+       <br>Aqui ID de Colegio<br><br>
+       <label for="anno">Anno:</label><br>
+       <input id="puntaje" type="text" name="puntaje"><br><br>
+       <label for="nivel">Nivel:</label><br>
+       <select name="nivel">
+         <option value="3">2° Medio</option>
+         <option value="2">8° Basico</option>
+         <option value="1">4° Basico</option>
+       </select><br><br>
+       <input type="submit" value="Enviar">
+     </form>
+
+</body>
+
+</html>
